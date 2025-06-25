8a872ef4fd96c229c495f293f647621f7e3a077d
Sevitha27 <sevitha.n03@gmail.com>
2025-06-25 17:21:06 +0530

    Relationship with -User, Skill, Phase for Vendor Entity updated

--- src/main/java/com/lowes/entity/Vendor.java
+++ src/main/java/com/lowes/entity/Vendor.java
@@ -7,6 +7,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.util.List;
+import java.util.UUID;
 
 @NoArgsConstructor
 @AllArgsConstructor
@@ -17,8 +18,39 @@ public class Vendor {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private UUID id;
 
-    @ManyToMany(mappedBy = "vendors")
-    private List<User> users;
+    private String companyName;
+
+    private String experience;
+
+    private Boolean available;
+
+    private Boolean approved;
+
+    @ManyToMany
+    @JoinTable(
+            name = "vendor_skills",
+            joinColumns = @JoinColumn(name = "vendor_id"),
+            inverseJoinColumns = @JoinColumn(name = "skill_id")
+    )
+    private List<Skill> skills;
+
+    @OneToMany(mappedBy = "vendor", cascade = CascadeType.ALL)
+    private List<VendorReview> reviews;
+
+    @ManyToMany
+    @JoinTable(
+            name = "vendor_customers",
+            joinColumns = @JoinColumn(name = "vendor_id"),
+            inverseJoinColumns = @JoinColumn(name = "customer_id")
+    )
+    private List<User> customers;
+
+    @OneToOne
+    @JoinColumn(name = "user_id") // this is the vendor's user account
+    private User user;
+
+    @OneToMany(mappedBy = "vendor", cascade = CascadeType.ALL)
+    private List<Phase> assignedPhases;
 }
