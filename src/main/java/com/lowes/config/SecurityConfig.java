c3dc57ccf718ea4644a4a8330c857c82f34376b3
Sevitha27 <sevitha.n03@gmail.com>
2025-06-28 20:34:28 +0530

    Security Config Updated - It enables testing without authentication as well

--- src/main/java/com/lowes/config/SecurityConfig.java
+++ src/main/java/com/lowes/config/SecurityConfig.java
@@ -41,6 +41,23 @@ public class SecurityConfig {
             "/auth/register", "/auth/login", "/auth/refreshAccessToken"
     };
 
+//For testing without authentication: you may uncomment the required methods below as needed.
+
+//    // Public API DELETE Endpoints to be Added here
+//    private final String[] PUBLIC_DELETE_ENDPOINTS = {
+//
+//    };
+//
+//    // Public API PUT Endpoints to be Added here
+//    private final String[] PUBLIC_PUT_ENDPOINTS = {
+//
+//    };
+//
+//    // Public API PATCH Endpoints to be Added here
+//    private final String[] PUBLIC_PATCH_ENDPOINTS = {
+//
+//    };
+
     private final List<String> ALLOWED_ORIGIN_URI = List.of(
             "http://localhost:5173", "http://localhost:5174"
     );
@@ -70,6 +87,11 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(HttpMethod.GET, PUBLIC_GET_ENDPOINTS).permitAll()
                         .requestMatchers(HttpMethod.POST, PUBLIC_POST_ENDPOINTS).permitAll()
+                        //For testing without Authentication: you may uncomment the required methods below as needed.
+
+//                        .requestMatchers(HttpMethod.DELETE, PUBLIC_DELETE_ENDPOINTS).permitAll()
+//                        .requestMatchers(HttpMethod.PATCH, PUBLIC_PATCH_ENDPOINTS).permitAll()
+//                        .requestMatchers(HttpMethod.PUT, PUBLIC_PUT_ENDPOINTS).permitAll()
                         .anyRequest().authenticated()
                 )
                 .sessionManagement(session -> session
