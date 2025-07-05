29c7276999b75906d88daf940af3db0e60489db6
Mukund Rao <officialmukundrao@gmail.com>
2025-07-05 14:48:54 +0530

    added preauthorize in material controller and phase material controller

--- src/main/java/com/lowes/controller/MaterialController.java
+++ src/main/java/com/lowes/controller/MaterialController.java
@@ -9,6 +9,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -22,6 +23,7 @@ public class MaterialController {
     Logger logger = LoggerFactory.getLogger(MaterialController.class);
 
 
+    @PreAuthorize("hasRole('ROLE_CUSTOMER)")
     @GetMapping("/user/materials")
     public ResponseEntity getExistingMaterialsByPhaseType(@RequestParam("phaseType") PhaseType phaseType){
         try{
