8ab18ac992857000011f722ede1612984613e116
VinutaSBhat <121873083+VinutaSBhat@users.noreply.github.com>
2025-07-16 20:30:38 +0530

    working

--- src/main/java/com/lowes/entity/Phase.java
+++ src/main/java/com/lowes/entity/Phase.java
@@ -15,6 +15,7 @@ import java.util.UUID;
 
 
 @Data
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @Setter
--- src/main/java/com/lowes/repository/RoomRepository.java
+++ src/main/java/com/lowes/repository/RoomRepository.java
@@ -3,12 +3,13 @@ package com.lowes.repository;
 
 import com.lowes.entity.Room;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface RoomRepository extends JpaRepository<Room, UUID> {
     List<Room> findByProjectId(UUID projectId);
+
 }
 
 
